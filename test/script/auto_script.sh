75c72f8c16764432608e8e7990f69c730c547e97
chencjcj <90088414+chencjcj@users.noreply.github.com>
2025-04-11 14:03:49 +0800

    Add code format auto-check in CI (#102)

--- test/script/auto_script.sh
+++ test/script/auto_script.sh
@@ -20,61 +20,63 @@ if [ $? -ne 0 ]; then
 fi
 
 
-mpirun -np 8 ./test_sendrecv -b 128M -e 2G -f 2 -p 1
-if [ $? -ne 0 ]; then
-    echo "test_sendrecv execution failed!"
-    exit 1
-fi
-
-mpirun -np 8 ./test_allreduce -b 128M -e 2G -f 2 -p 1
-if [ $? -ne 0 ]; then
-    echo "test_allreduce execution failed!"
-    exit 1
-fi
-
-mpirun -np 8 ./test_allgather -b 128M -e 2G -f 2 -p 1
-if [ $? -ne 0 ]; then
-    echo "test_allgather execution failed!"
-    exit 1
-fi
-
-mpirun -np 8 ./test_reducescatter -b 128M -e 2G -f 2 -p 1
-if [ $? -ne 0 ]; then
-    echo "test_reducescatter execution failed!"
-    exit 1
-fi
-
-mpirun -np 8 ./test_alltoall -b 128M -e 2G -f 2 -p 1
+mpirun -np 8   ./test_alltoall -b 128M -e 1G -f 2 -p 1
 if [ $? -ne 0 ]; then
     echo "test_alltoall execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_alltoallv -b 128M -e 2G -f 2 -p 1
+mpirun -np 8 ./test_alltoallv -b 128M -e 1G -f 2 -p 1
 if [ $? -ne 0 ]; then
     echo "test_alltoallv execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_broadcast -b 128M -e 2G -f 2 -r 0 -p 1
+mpirun -np 8 ./test_sendrecv -b 128M -e 1G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_sendrecv execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_allreduce -b 128M -e 1G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_allreduce execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_allgather -b 128M -e 1G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_allgather execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_reducescatter -b 128M -e 1G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_reducescatter execution failed!"
+    exit 1
+fi
+
+
+
+mpirun -np 8 ./test_broadcast -b 128M -e 1G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_broadcast execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_gather -b 128M -e 2G -f 2 -r 0 -p 1
+mpirun -np 8 ./test_gather -b 128M -e 1G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_gather execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_scatter -b 128M -e 2G -f 2 -r 0 -p 1
+mpirun -np 8 ./test_scatter -b 128M -e 1G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_scatter execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_reduce -b 128M -e 2G -f 2 -r 0 -p 1
+mpirun -np 8 ./test_reduce -b 128M -e 1G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_reduce execution failed!"
     exit 1
